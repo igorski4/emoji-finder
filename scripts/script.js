a88c6993aac1da5af849b3c3b9e2ee04044aef31
Igor Karpov <karpov_igorspb@mail.ru>
2023-01-29 21:10:52 +0300

    add search

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,56 @@
 import { data } from "./data.js";
 
 let input = document.querySelector(".header__input");
-// let title = document.querySelector(".header__title");
-// input.addEventListener("input", () => (title.innerHTML = input.value));
+let gridEmoji = document.querySelector(".main__grid");
 
-for (let el of data) {
+//Добавление одной карточки с эмодзи
+const addCard = (symbol, title, keywords) => {
   let divEmoji = document.createElement("div");
   divEmoji.className = "grid__item item";
 
   let pSymbol = document.createElement("p");
-  pSymbol.innerHTML = el.symbol;
+  pSymbol.innerHTML = symbol;
   pSymbol.className = "item__symbol";
   divEmoji.append(pSymbol);
 
   let pTitle = document.createElement("h2");
-  pTitle.innerHTML = el.title;
+  pTitle.innerHTML = title;
   pTitle.className = "item__title";
   divEmoji.append(pTitle);
 
   let pKeywords = document.createElement("p");
-  pKeywords.innerHTML = el.keywords;
+  pKeywords.innerHTML = keywords;
   pKeywords.className = "item__keywords";
   divEmoji.append(pKeywords);
 
-  let gridEmoji = document.querySelector(".main__grid");
   gridEmoji.append(divEmoji);
-}
+};
+
+//Добавление всех карточек
+data.forEach((el) => addCard(el.symbol, el.title, el.keywords));
+
+//Обработка введенной строки
+const processingString = (str) => {
+  let arr = str.trim().split(" ");
+  let outputArr = [];
+  arr.forEach((el) => {
+    if (!outputArr.includes(el) && el !== "") outputArr.push(el);
+  });
+  return outputArr;
+};
+
+//Обработка ввода текста в input
+input.addEventListener("input", () => {
+  let arrUniqueWords = processingString(input.value);
+  console.log(arrUniqueWords);
+  gridEmoji.innerHTML = "";
+  data.forEach((elem) => {
+    let stringKeys = elem.title + " " + elem.keywords;
+    let countFinding = arrUniqueWords.reduce(
+      (acc, cur) => acc + stringKeys.includes(cur),
+      0
+    );
+    if (countFinding === arrUniqueWords.length)
+      addCard(elem.symbol, elem.title, elem.keywords);
+  });
+});
